dfc749570663d11b3cec63549e667bdfb45a55a0
NabiyevSaleh <saleh.nabiyev@mail.ru>
2023-05-04 16:49:48 +0400

    TASK01

---
+++ Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp25.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25.Models
+{
+    internal class Book: BaseAuditibleEntitiy
+    {
+       
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountPrice { get; set; }
+        public string Quantitiy { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        
+
+
+
+
+
+
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp25.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25.Models
+{
+    internal class Category: BaseAuditibleEntitiy
+    {
+        public string Name { get; set; }
+        public int ParentId { get; set; }
+        
+    }
+}
